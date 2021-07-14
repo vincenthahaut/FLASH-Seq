100fc7f8c3e343086ae979c1a8d8d85c020dde46
Radek91 <vincent.hahaut@gmail.com>
2021-07-14 11:41:38 +0200

    Added Alignment scripts

---
+++ align/2_SINGLE_END.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+
+# 1. PATH
+# The sample sheet only contains the FASTQ prefix - one per lane
+SAMPLESHEET="/home/vincent.hahaut/data_storage/210617_NB551561_0051_AHTYJYAFX2/ID.txt"
+OUT="/home/vincent.hahaut/data_storage/210617_NB551561_0051_AHTYJYAFX2/STAR/"
+IN="/home/vincent.hahaut/data_storage/210617_NB551561_0051_AHTYJYAFX2/"
+
+# 2. REFERENCES
+STAR_REF="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/STAR/"
+# STAR_REF="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/STAR_100bp/"	# To use when dealing with 100 bp reads
+GTF="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/GTF/gencode.v34.primary_assembly.annotation.gtf"
+GTF_CODING="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/GTF/gencode.v34.primary_assembly.annotation.protein_coding.gtf"
+GTF_EXON_CODING="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/GTF/gencode.v34.exons.gtf"
+GTF_INTRON_CODING="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/GTF/gencode.v34.coding.introns.gtf"
+ReSQCBED="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/BED/hg38_UCSC_gencodeV34.ReSQCBED.bed"
+BBDUK_REF="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/BBDUK/adapters.fa"
+
+# 3. BINARIES
+STAR="/home/vincent.hahaut/binaries/STAR-2.7.3a/bin/Linux_x86_64/STAR"
+BBDUK="/home/vincent.hahaut/data_storage/binaries/bbmap/bbduk.sh"
+BBSPLIT="/home/vincent.hahaut/data_storage/binaries/bbmap/bbsplit.sh"
+BBMAP_filter="/home/vincent.hahaut/data_storage/binaries/bbmap/filterbyname.sh"
+
+# 4. Run the pipeline for every sample
+cd "$IN"
+mkdir "$OUT"
+cd "$OUT"
+
+ulimit -n 100000
+STAR --genomeLoad LoadAndExit --genomeDir $STAR_REF --runThreadN 24
+
+cat "$SAMPLESHEET" |
+while IFS=$'\t' read -r -a myArray; do
+
+	# 0. Get the CELL ID and FASTQ
+	ID="${myArray[0]}"
+	FASTQ=$(echo $IN/out/$ID*R1*fastq.gz | sed 's/ /,/g')
+	echo "$ID"
+	mkdir "$ID"
+	cd "$ID"
+
+	# 1. Save the FASTQ
+	cp $(echo "$FASTQ" | sed 's/,/ /g') .
+	cat *fastq.gz > sample.fastq.gz
+
+	# 2. Trim Reads
+	# The following oligos have been added to the adapters.fa reference 
+   	# https://teichlab.github.io/scg_lib_structs/methods_html/SMART-seq_family.html
+    	# SS2/FS: AAGCAGTGGTATCAACGCAGAGT, GGTATCAACGCAGAGTACATGGG
+    	# SS3: ACGAGCATCAGCAGCATACGA, AGAGACAGATTGCGCAATG
+	if echo "$ID" | grep -q "SS3"
+	then
+	#### Remove UMI from 5'UMI reads from SS3 data and add them to the read name
+	# t = threads
+	# ktrim = right and left
+	# rcomp=f only look for the forward sequence not reverse
+	# k = min kmer length trim
+	# hdist = hamming distance for mismatches
+	# mink = min kmer length trim - at edges of fragments
+	# hdist2 = for kmer < k, use this hamming distance
+	# minlength = minimal read length or discard
+	# tbo = trim on where paired reads overlap 
+	# 2.1. Remove UMI
+	# Could leave some reads with .*ATTGCGCAATG{umi} but these are rare and also left by zUMIs
+    	umi_tools extract --bc-pattern="^(?P<discard_1>ATTGCGCAATG){s<=2}(?P<umi_1>.{8})" --stdin=sample.fastq.gz --stdout=umi.fastq --extract-method=regex
+	# 2.2. Subset non-UMI reads and append them to trimmed umi.fastq
+	awk 'NR%4==1{print}' umi.fastq | sed 's/_........//g' | sed 's/^@//g' > names.txt
+	$BBMAP_filter in=sample.fastq.gz out=non_umi.fastq names=names.txt include=f overwrite=t
+  	cat non_umi.fastq umi.fastq > preprocessed.fq
+    	# 2.3. Trim Reads
+    	# Remove F/R sequencing adapters
+    	$BBDUK -Xmx48g in=preprocessed.fq out=cleaned.polyA.fastq t=32 ktrim=r rcomp=f literal=AAAAAAA hdist=0 k=6
+    	$BBDUK -Xmx48g in=cleaned.polyA.fastq out=cleaned.left.fastq t=32 ktrim=l ref="$BBDUK_REF" k=23 minlength=50 mink=7 hdist=1 hdist2=0 tbo
+    	$BBDUK -Xmx48g in=cleaned.left.fastq out=cleaned.fastq t=32 ktrim=r ref="$BBDUK_REF" k=23 minlength=50 mink=7 hdist=1 hdist2=0 tbo
+    	else
+    	#### Takara, SS2, FS
+    	# 2.3. Trim Reads
+    	$BBDUK -Xmx48g in=sample.fastq.gz out=cleaned.left.fastq t=32 ktrim=l ref="$BBDUK_REF" k=23 minlength=60 mink=7 hdist=1 hdist2=0 tbo
+    	$BBDUK -Xmx48g in=cleaned.left.fastq out=cleaned.fastq t=32 ktrim=r ref="$BBDUK_REF" k=23 minlength=60 mink=7 hdist=1 hdist2=0 tbo
+	fi
+	gzip -c cleaned.fastq > cleaned.fastq.gz
+
+	mkdir FASTQ
+	MYFASTQ="$OUT"/"$ID"/cleaned.fastq.gz
+	scp "$MYFASTQ" FASTQ/
+
+	# 3. Align the Data
+	# TO OUTPUT EVERYTHING: --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread 0 --outFilterMatchNmin 0 --outReadsUnmapped Fastx
+	mkdir STAR
+	$STAR --runThreadN 30 --limitBAMsortRAM 20000000000 --genomeLoad LoadAndKeep --genomeDir "$STAR_REF" --readFilesIn "$MYFASTQ" --readFilesCommand zcat --quantMode TranscriptomeSAM --limitSjdbInsertNsj 2000000 --outFilterIntronMotifs RemoveNoncanonicalUnannotated --outSAMtype BAM SortedByCoordinate --outFileNamePrefix STAR/"$ID"_
+
+	# 4. Samtools
+	samtools view -@ 30 -Sb -F 260 STAR/"$ID"_Aligned.sortedByCoord.out.bam > STAR/"$ID"_Aligned.sortedByCoord.filtered.bam
+	samtools index STAR/"$ID"_Aligned.sortedByCoord.filtered.bam
+
+	samtools view -@ 30 -Sb -F 260 STAR/"$ID"_Aligned.toTranscriptome.out.bam > STAR/"$ID"_Aligned.toTranscriptome.filtered.bam
+	samtools sort -@ 30 STAR/"$ID"_Aligned.toTranscriptome.filtered.bam -o STAR/"$ID"_Aligned.toTranscriptome.filtered.sorted.bam
+	samtools index STAR/"$ID"_Aligned.toTranscriptome.filtered.sorted.bam
+	rm STAR/"$ID"_Aligned.toTranscriptome.filtered.bam
+
+	# 5. Read Count using FeatureCounts
+	# -T = Threads
+	# -t = Summarise exon results mapping ..
+	# -g = .. to gene IDs
+	# -a = reference
+	# -o = OUTPUT
+	mkdir FEATURECOUNTS
+	# On EXON - ALL GENES
+	featureCounts -T 1 -t exon -g gene_name -a "$GTF" -o FEATURECOUNTS/"$ID"_ReadCount.featureCounts.gencode.txt STAR/"$ID"_Aligned.sortedByCoord.filtered.bam &
+	# On GENE - ALL GENES
+	featureCounts -T 1 -t gene -g gene_name -a "$GTF" -o FEATURECOUNTS/"$ID"_ReadCount.featureCounts.gencode.GENE.txt STAR/"$ID"_Aligned.sortedByCoord.filtered.bam &
+	# On EXON - CODING
+	featureCounts -T 1 -t exon -g gene_name -a "$GTF_CODING" -o FEATURECOUNTS/"$ID"_ReadCount_coding.featureCounts.gencode.txt STAR/"$ID"_Aligned.sortedByCoord.filtered.bam &
+	# On GENE - CODING
+	featureCounts -T 1 -t gene -g gene_name -a "$GTF_CODING" -o FEATURECOUNTS/"$ID"_ReadCount_coding.featureCounts.gencode.GENE.txt STAR/"$ID"_Aligned.sortedByCoord.filtered.bam &
+
+	# 6. Downsampling
+	mkdir DOWNSAMPLE
+	cd DOWNSAMPLE
+	for DOWN in 50000 100000 250000 500000 1000000
+	do
+	if [ `zcat "$MYFASTQ" | grep -c ^@`  -gt "$DOWN" ]
+	then
+	# If multiple downsamplings were needed
+	for TIMES in 1
+	do
+	mkdir DOWN_"$DOWN"_"$TIMES"
+	cd DOWN_"$DOWN"_"$TIMES"
+	if [ $TIMES -eq 1 ]
+	then
+	seqtk sample -s42 "$MYFASTQ" "$DOWN" > downsampled."$TIMES".fastq
+	fi
+	#
+	# 6.2. STAR Mapping
+	mkdir FEATURECOUNTS
+	$STAR --runThreadN 30 --limitBAMsortRAM 20000000000 --genomeLoad LoadAndKeep --genomeDir "$STAR_REF" --quantMode TranscriptomeSAM --readFilesIn downsampled."$TIMES".fastq --readFilesCommand cat --limitSjdbInsertNsj 2000000 --outFilterIntronMotifs RemoveNoncanonicalUnannotated --outSAMtype BAM SortedByCoordinate --outFileNamePrefix "$ID"_downsampled_"$DOWN"_"$TIMES"_
+	samtools view -@ 30 -Sb -F 260 "$ID"_downsampled_"$DOWN"_"$TIMES"_Aligned.sortedByCoord.out.bam > "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam
+	samtools index "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam
+
+	# 6.3. FeatureCounts Quantification
+	# Many of the following lines are not directly used in the paper but were used to explore the results.
+	# On EXON - ALL GENES
+	featureCounts -T 1 -t exon -g gene_name -a "$GTF" -o FEATURECOUNTS/"$ID"_ReadCount.featureCounts.gencode."$DOWN"."$TIMES".txt "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam &
+	# On GENE - ALL GENES
+	featureCounts -T 1 -t gene -g gene_name -a "$GTF" -o FEATURECOUNTS/"$ID"_ReadCount.featureCounts.gencode.GENE."$DOWN"."$TIMES".txt "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam &
+	# On EXON - CODING
+	featureCounts -T 1 -t exon -g gene_name -a "$GTF_CODING" -o FEATURECOUNTS/"$ID"_ReadCount_coding.featureCounts.gencode."$DOWN"."$TIMES".txt "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam &
+	# On GENE - CODING
+	featureCounts -T 1 -t gene -g gene_name -a "$GTF_CODING" -o FEATURECOUNTS/"$ID"_ReadCount_coding.featureCounts.gencode.GENE."$DOWN"."$TIMES".txt "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam &
+	# On EXON - CODING
+	featureCounts -T 1 -t exon -g gene_id -a "$GTF_EXON_CODING" -o FEATURECOUNTS/"$ID"_ReadCount_coding.featureCounts.gencode.exonOnly."$DOWN"."$TIMES".txt "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam &
+	# On INTRON - CODING
+	# Read need to be fully enclosed in a intron to be called intronic
+	featureCounts -T 1 -t intron -g gene_id -a "$GTF_INTRON_CODING" --fracOverlap 1 -o FEATURECOUNTS/"$ID"_ReadCount_coding.featureCounts.gencode.intronOnly."$DOWN"."$TIMES".txt "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam &
+	# On EXON - CODING
+	featureCounts -T 1 -t exon -g exon_id -a "$GTF_EXON_CODING" -o FEATURECOUNTS/"$ID"_ReadCount_coding.featureCounts.gencode.exonOnly.SummarizedToExonID."$DOWN"."$TIMES".txt "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam &
+	# On INTRON - CODING
+	featureCounts -T 1 -t intron -g intron_id -a "$GTF_INTRON_CODING" --fracOverlap 1 -o FEATURECOUNTS/"$ID"_ReadCount_coding.featureCounts.gencode.intronOnly.SummarizedToIntronID."$DOWN"."$TIMES".txt "$ID"_downsampled_"$DOWN"_Aligned.sortedByCoord.filtered."$TIMES".bam &
+
+	# 6.4. Clean-up
+	rm *_Aligned.filtered.bam *_Aligned.sortedByCoord.out.bam *_Log.out *Log.progress.out *_SJ.out.tabs
+	cd ..
+	done
+	fi
+	done
+	cd ..
+
+	# 7. GeneBodyCoverage
+	mkdir ReSQC
+	geneBody_coverage.py -r "$ReSQCBED" -i STAR/"$ID"_Aligned.sortedByCoord.filtered.bam -o ReSQC/"$ID"_geneBody.all &
+
+	# 8. ReSQC Statistics
+	read_distribution.py -i STAR/"$ID"_Aligned.sortedByCoord.filtered.bam -r "$ReSQCBED" > ReSQC/"$ID"_readDistribution.txt &
+
+	# 9. Clean-up
+	rm *fastq*
+	rm STAR/"$ID"_Aligned.sortedByCoord.filtered.bam.tmp
+	rm FASTQC/*zip
+	rm log.txt
+
+	cd "$OUT"
+done
+
+STAR --genomeLoad Remove --genomeDir "$STAR_REF"
+
+rm ./*/DOWNSAMPLE/*/down*fastq
---
+++ align/4_SINGLE_END_UMI.sh
@@ -0,0 +1,238 @@
+#!/bin/bash
+
+# PATH
+SAMPLESHEET="/home/vincent.hahaut/data_storage/210322_NB551561_0044_AHNTL2AFX2/ID_all.txt"
+OUT="/home/vincent.hahaut/data_storage/210322_NB551561_0044_AHNTL2AFX2/STAR/"
+IN="/home/vincent.hahaut/data_storage/210322_NB551561_0044_AHNTL2AFX2/"
+
+# REFERENCES
+#STAR_REF="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/STAR/"
+STAR_REF="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/STAR_100bp/" # To use whith 100 bp reads
+GTF="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/GTF/gencode.v34.primary_assembly.annotation.gtf"
+ReSQCBED="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/BED/hg38_UCSC_gencodeV34.ReSQCBED.bed"
+BBDUK_REF="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/BBDUK/adapters.fa"
+GTF_EXONINTRON="/home/vincent.hahaut/data_storage/REFERENCES/hsap_fastsmart/REFERENCE/GTF/gencode.v34.exonsANDintrons.gtf"
+
+# BINARIES
+STAR="/home/vincent.hahaut/binaries/STAR-2.7.3a/bin/Linux_x86_64/STAR"
+BBDUK="/home/vincent.hahaut/data_storage/binaries/bbmap/bbduk.sh"
+TRACER="/home/vincent.hahaut/data_storage/binaries/tracer/tracer"
+BBMAP_filter="/home/vincent.hahaut/data_storage/binaries/bbmap/filterbyname.sh"
+
+mkdir "$OUT"
+cd "$OUT"
+
+ulimit -n 100000
+STAR --genomeLoad LoadAndExit --genomeDir $STAR_REF
+
+cat "$SAMPLESHEET" |
+while IFS=$'\t' read -r -a myArray; do
+
+	# 0. Get the variables and FASTQ
+	ID="${myArray[0]}"
+
+	# In case multiple folder separated by , are provided.
+	FASTQ_R1=$(echo $IN | tr "," "\n" | awk -v ID=$ID '{print $1"/out/"ID"*_R1_*fastq.gz"}')
+	#FASTQ_R1="$IN"/out/"$ID".fq.gz
+
+	mkdir "$ID"
+	cd "$ID"
+	# Pull all FASTQ R1 to the folder:
+	cat $FASTQ_R1 > sample.R1.fastq.gz
+
+	echo "$ID"
+
+	# 1. Get UMI
+	# Depending on the cell ID - extract the right UMI - Remove adapter/spacer/GGG 
+	mkdir FASTQ 
+
+        if echo "$ID" | grep -q "Spacer1"
+        then
+        echo "TSO-Spacer1"
+        umi_tools extract --bc-pattern="^(?P<discard_1>.*GT)(?P<umi_1>.{8})(?P<discard_2>CAGCAGG)(?P<discard_3>G{0,2})" --stdin=sample.R1.fastq.gz --stdout=FASTQ/umi.R1.fq --extract-method=regex
+	elif echo "$ID" | grep -q "Spacer2"
+        then
+        echo "TSO-Spacer2"
+        umi_tools extract --bc-pattern="^(?P<discard_1>.*GT)(?P<umi_1>.{8})(?P<discard_2>ATAACGG)(?P<discard_3>G{0,2})" --stdin=sample.R1.fastq.gz --stdout=FASTQ/umi.R1.fq --extract-method=regex
+	elif echo "$ID" | grep -q "CTAAC"
+        then
+        echo "TSO-CTAAC"
+        umi_tools extract --bc-pattern="^(?P<discard_1>.*GT)(?P<umi_1>.{8})(?P<discard_2>CTAACGG)(?P<discard_3>G{0,2})" --stdin=sample.R1.fastq.gz --stdout=FASTQ/umi.R1.fq --extract-method=regex
+	elif echo "$ID" | grep -q "ATGAC"
+        then
+        echo "TSO-ATGAC"
+        umi_tools extract --bc-pattern="^(?P<discard_1>.*GT)(?P<umi_1>.{8})(?P<discard_2>ATGACGG)(?P<discard_3>G{0,2})" --stdin=sample.R1.fastq.gz --stdout=FASTQ/umi.R1.fq --extract-method=regex
+	elif echo "$ID" | grep -q "CTGAC"
+        then
+        echo "TSO-CTGAC"
+        umi_tools extract --bc-pattern="^(?P<discard_1>.*GT)(?P<umi_1>.{8})(?P<discard_2>CTGACGG)(?P<discard_3>G{0,2})" --stdin=sample.R1.fastq.gz --stdout=FASTQ/umi.R1.fq --extract-method=regex
+	elif echo "$ID" | grep -q "AAGCA"
+        then
+        echo "TSO-AAGCA"
+        umi_tools extract --bc-pattern="^(?P<discard_1>.*GT)(?P<umi_1>.{8})(?P<discard_2>AAGCAGG)(?P<discard_3>G{0,2})" --stdin=sample.R1.fastq.gz --stdout=FASTQ/umi.R1.fq --extract-method=regex
+	elif echo "$ID" | grep -q "CATCA"
+        then
+        echo "TSO-CATCA"
+        umi_tools extract --bc-pattern="^(?P<discard_1>.*GT)(?P<umi_1>.{8})(?P<discard_2>CATCAGG)(?P<discard_3>G{0,2})" --stdin=sample.R1.fastq.gz --stdout=FASTQ/umi.R1.fq --extract-method=regex
+	elif echo "$ID" | grep -qE "FStso|Fstso"
+	then
+	echo "FS-TSO"
+   	umi_tools extract --bc-pattern="^(?P<discard_1>.+CAACGCAGAGT){s<=1}(?P<umi_1>.{8})(?P<discard_2>G{0,4})" --stdin=sample.R1.fastq.gz --stdout=FASTQ/umi.R1.fq --extract-method=regex
+	elif echo "$ID" | grep -qE "5_SS3|6_SS3|SS3fwd"
+	then
+	echo "SS3-TSO"
+        umi_tools extract --bc-pattern="^(?P<discard_1>.*ATTGCGCAATG){s<=1}(?P<umi_1>.{8})(?P<discard_2>G{0,4})" --stdin=sample.R1.fastq.gz --stdout=FASTQ/umi.R1.fq --extract-method=regex
+	fi
+
+	# UMI-tools has a filtered-out option but does not work here. I think it expect paired ends for this to work.
+	# Split UMI - internal using BBMAP
+	awk 'NR%4==1{print}' FASTQ/umi.R1.fq | sed 's/_........//g' | sed 's/^@//g' > names.txt
+	$BBMAP_filter in=sample.R1.fastq.gz out=FASTQ/internal.R1.fq names=names.txt include=f overwrite=t
+	rm names.txt
+
+	cat FASTQ/umi.R1.fq FASTQ/internal.R1.fq > FASTQ/allreads.R1.fq
+
+	# 2. Remove leftover of adapters
+        # Remove F/R sequencing adapters
+  	# t = threads
+	# ktrim = right and left
+	# rcomp=f only look for the forward sequence not reverse
+	# k = min kmer length trim
+	# hdist = hamming distance for mismatches
+	# mink = min kmer length trim - at edges of fragments
+	# hdist2 = for kmer < k, use this hamming distance
+	# minlength = minimal read length or discard
+	# tbo = trim on where paired reads overlap 
+	$BBDUK -Xmx48g in=FASTQ/allreads.R1.fq out=FASTQ/allreads.R1.2.fq t=32 ktrim=r ref="$BBDUK_REF" k=23 minlength=60 mink=10 hdist=1
+	$BBDUK -Xmx48g in=FASTQ/allreads.R1.2.fq out=FASTQ/allreads.R1.trim.fq t=32 ktrim=l ref="$BBDUK_REF" k=23 minlength=60 mink=10 hdist=1
+
+  	$BBDUK -Xmx48g in=FASTQ/umi.R1.fq out=FASTQ/umi.R1.2.fq t=32 ktrim=r ref="$BBDUK_REF" k=18 minlength=40 mink=10 hdist=1
+	$BBDUK -Xmx48g in=FASTQ/umi.R1.2.fq out=FASTQ/umi.R1.trim.fq t=32 ktrim=l ref="$BBDUK_REF" k=18 minlength=40 mink=10 hdist=1
+
+  	$BBDUK -Xmx48g in=FASTQ/internal.R1.fq out=FASTQ/internal.R1.2.fq t=32 ktrim=r ref="$BBDUK_REF" k=23 minlength=60 mink=10 hdist=1
+	$BBDUK -Xmx48g in=FASTQ/internal.R1.2.fq out=FASTQ/internal.R1.trim.fq t=32 ktrim=l ref="$BBDUK_REF" k=23 minlength=60 mink=10 hdist=1
+
+	rm FASTQ/allreads.R1.2.fq FASTQ/internal.R1.2.fq FASTQ/umi.R1.2.fq
+
+	gzip -c FASTQ/allreads.R1.trim.fq > FASTQ/allreads.R1.fq.gz
+	gzip -c FASTQ/umi.R1.trim.fq > FASTQ/umi.R1.fq.gz
+	gzip -c FASTQ/internal.R1.trim.fq > FASTQ/internal.R1.fq.gz
+	rm FASTQ/allreads.R1.trim.fq FASTQ/umi.R1.trim.fq FASTQ/internal.R1.trim.fq
+
+	# REDEFINED VARIABLES
+	MYFASTQ_R1="$OUT"/"$ID"/FASTQ/allreads.R1.fq.gz
+	MYFASTQ_UMI_R1="$OUT"/"$ID"/FASTQ/umi.R1.fq.gz
+	MYFASTQ_INTERNAL_R1="$OUT"/"$ID"/FASTQ/internal.R1.fq.gz
+
+	# 3. Align the Data
+	mkdir STAR
+
+	# To validate trimming results ==> Output no trimmed as well
+	# OUTPUT EVERYTHING --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread 0 --outFilterMatchNmin 0 --outReadsUnmapped Fastx
+	$STAR --runThreadN 30 --limitBAMsortRAM 20000000000 --genomeLoad LoadAndKeep --genomeDir "$STAR_REF" --readFilesIn sample.R1.fastq.gz --readFilesCommand zcat --limitSjdbInsertNsj 2000000 --outFilterIntronMotifs RemoveNoncanonicalUnannotated --outSAMtype BAM SortedByCoordinate --outFileNamePrefix STAR/"$ID"_ALLnoTrim_
+	samtools view -@ 30 -Sb -F 260 -q 5 STAR/"$ID"_ALLnoTrim_Aligned.sortedByCoord.out.bam > STAR/"$ID"_ALLnoTrim_Aligned.sortedByCoord.filtered.bam
+	samtools index STAR/"$ID"_ALLnoTrim_Aligned.sortedByCoord.filtered.bam
+
+	for READS in "$MYFASTQ_R1" "$MYFASTQ_UMI_R1" "$MYFASTQ_INTERNAL_R1"
+	do
+
+	if echo "$READS" | grep -q "allreads"
+	then
+	TYPE="ALL"
+	elif echo "$READS" | grep -q "umi"
+	then
+	TYPE="UMI"
+	elif echo "$READS" | grep -q "internal"
+	then
+	TYPE="INTERNAL"
+	fi
+
+	echo "$READS"
+	echo $TYPE
+
+	# 3.1. MAP
+	$STAR --runThreadN 30 --limitBAMsortRAM 20000000000 --genomeLoad LoadAndKeep --genomeDir "$STAR_REF" --readFilesIn "$READS" --quantMode TranscriptomeSAM --readFilesCommand zcat --limitSjdbInsertNsj 2000000 --outFilterIntronMotifs RemoveNoncanonicalUnannotated --outSAMtype BAM SortedByCoordinate --outFileNamePrefix STAR/"$ID"_"$TYPE"_
+
+	# 3.2. Samtools
+	samtools view -@ 30 -Sb -F 260 -q 5 STAR/"$ID"_"$TYPE"_Aligned.sortedByCoord.out.bam > STAR/"$ID"_"$TYPE"_Aligned.sortedByCoord.filtered.bam
+	samtools index STAR/"$ID"_"$TYPE"_Aligned.sortedByCoord.filtered.bam
+
+	samtools view -@ 30 -Sb -F 260 -q 5 STAR/"$ID"_"$TYPE"_Aligned.toTranscriptome.out.bam > STAR/"$ID"_"$TYPE"_Aligned.toTranscriptome.filtered.bam
+	samtools sort -@ 30 STAR/"$ID"_"$TYPE"_Aligned.toTranscriptome.filtered.bam -o STAR/"$ID"_"$TYPE"_Aligned.toTranscriptome.filtered.sorted.bam
+	samtools index STAR/"$ID"_"$TYPE"_Aligned.toTranscriptome.filtered.sorted.bam
+	rm STAR/"$ID"_"$TYPE"_Aligned.toTranscriptome.filtered.bam
+
+	# 3.3.1. FeatureCounts
+	mkdir FEATURECOUNTS
+	if [ "$TYPE" != "UMI" ]
+	then
+	featureCounts -T 15 -t gene -g gene_name -a "$GTF" -o FEATURECOUNTS/"$ID"_"$TYPE"_featureCounts.gencode.GENE.txt STAR/"$ID"_"$TYPE"_Aligned.sortedByCoord.filtered.bam &
+	featureCounts -T 15 -t exon -g gene_name -a "$GTF" -o FEATURECOUNTS/"$ID"_"$TYPE"_featureCounts.gencode.EXON.txt STAR/"$ID"_"$TYPE"_Aligned.sortedByCoord.filtered.bam &
+	fi
+
+	# 3.3.2. FeatureCounts - BAM file
+	# For strand-invasion analysis
+	if [ "$TYPE" == "UMI" ]
+	then
+	# featureCounts -T 30 -t gene -g gene_name -s 0 --fracOverlap 0.25 -a "$GTF" -R BAM -o FEATURECOUNTS/"$ID"_unstranded.Aligned.sorted.bam STAR/"$ID"_UMI_Aligned.sortedByCoord.filtered.bam
+	featureCounts -T 30 -t all -g feature -s 0 --largestOverlap --fracOverlap 0.25 -a "$GTF_EXONINTRON" -R BAM -o FEATURECOUNTS/"$ID"_UMI_featureCounts_ExonIntrons STAR/"$ID"_UMI_Aligned.sortedByCoord.filtered.bam 
+	cd "$OUT"
+	fi
+
+	# 3.4. Downsampling
+	mkdir DOWNSAMPLE
+	cd DOWNSAMPLE
+	for DOWN in 10000 25000 50000 75000 100000 125000 250000
+	do
+	if [ `zcat "$READS" | grep -c ^@`  -gt "$DOWN" ]
+	then
+	mkdir DOWN_"$DOWN"
+	cd DOWN_"$DOWN"
+	seqtk sample -s42 "$READS" "$DOWN" > "$TYPE"."$DOWN".R1.fastq
+
+	# MAP
+	$STAR --runThreadN 30 --limitBAMsortRAM 20000000000 --genomeLoad LoadAndKeep --genomeDir "$STAR_REF" --readFilesIn "$TYPE"."$DOWN".R1.fastq --readFilesCommand cat --limitSjdbInsertNsj 2000000 --outFilterIntronMotifs RemoveNoncanonicalUnannotated --outSAMtype BAM SortedByCoordinate --outFileNamePrefix "$ID"_"$TYPE"_"$DOWN"_
+	samtools view -@ 30 -Sb -F 260 -q 5 "$ID"_"$TYPE"_"$DOWN"_Aligned.sortedByCoord.out.bam > "$ID"_"$TYPE"_"$DOWN"_Aligned.sorted.bam
+	samtools index "$ID"_"$TYPE"_"$DOWN"_Aligned.sorted.bam
+	rm "$ID"_"$TYPE"_"$DOWN"_Aligned.sortedByCoord.out.bam
+
+	# Quantify
+	mkdir FEATURECOUNTS
+	if [ "$TYPE" = "UMI" ]
+	then
+	featureCounts -T 30 -t exon -g gene_name -s 1 --fracOverlap 0.25 -a "$GTF" -R BAM -o FEATURECOUNTS/"$ID"_"$TYPE"_"$DOWN".feature.txt "$ID"_"$TYPE"_"$DOWN"_Aligned.sorted.bam
+	samtools view -h FEATURECOUNTS/"$ID"_UMI_"$DOWN"_Aligned.sorted.bam.featureCounts.bam | awk -F'\t' '$1~/_.{8}$|@SQ|@HD/{print $0}' | samtools view -Sb - > "$ID"_assignedUMI_"$DOWN".bam 
+	samtools sort -@ 30 "$ID"_assignedUMI_"$DOWN".bam  > "$ID"_assignedUMI_"$DOWN".sorted.bam
+	rm FEATURECOUNTS/"$ID"_UMI_"$DOWN"_Aligned.sorted.bam.featureCounts.bam
+	samtools index "$ID"_assignedUMI_"$DOWN".sorted.bam
+	umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "$ID"_assignedUMI_"$DOWN".sorted.bam -S FEATURECOUNTS/umi.counts."$DOWN".tsv.gz
+	else
+	featureCounts -T 15 -t gene -g gene_name -a "$GTF" -o FEATURECOUNTS/"$ID"_"$TYPE"_"$DOWN".feature.GENE.txt "$ID"_"$TYPE"_"$DOWN"_Aligned.sorted.bam &
+	featureCounts -T 15 -t exon -g gene_name -a "$GTF" -R BAM -o FEATURECOUNTS/"$ID"_"$TYPE"_"$DOWN".feature.EXON.txt "$ID"_"$TYPE"_"$DOWN"_Aligned.sorted.bam &
+	fi
+
+	cd "$OUT"/"$ID"/DOWNSAMPLE/
+
+	fi
+
+	done
+
+	cd "$OUT"/"$ID"/
+
+	done
+
+	# 7. GeneBodyCoverage
+	mkdir ReSQC
+	geneBody_coverage.py -r "$ReSQCBED" -i STAR/"$ID"_INTERNAL_Aligned.sortedByCoord.out.bam -o ReSQC/"$ID"_INTERNAL_geneBody.all &
+	geneBody_coverage.py -r "$ReSQCBED" -i STAR/"$ID"_ALL_Aligned.sortedByCoord.out.bam -o ReSQC/"$ID"_ALL_geneBody.all &
+	geneBody_coverage.py -r "$ReSQCBED" -i STAR/"$ID"_UMI_Aligned.sortedByCoord.out.bam -o ReSQC/"$ID"_UMI_geneBody.all &
+
+	# 8. ReSQC Statistics
+	read_distribution.py -i STAR/"$ID"_INTERNAL_Aligned.sortedByCoord.out.bam -r "$ReSQCBED" > ReSQC/"$ID"_INTERNAL_readDistribution.txt &
+	read_distribution.py -i STAR/"$ID"_ALL_Aligned.sortedByCoord.out.bam -r "$ReSQCBED" > ReSQC/"$ID"_ALL_readDistribution.txt &
+	read_distribution.py -i STAR/"$ID"_UMI_Aligned.sortedByCoord.out.bam -r "$ReSQCBED" > ReSQC/"$ID"_UMI_readDistribution.txt &
+
+	cd "$OUT"
+done
+
+STAR --genomeLoad Remove --genomeDir "$STAR_REF"
